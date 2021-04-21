141dfe6589a4de7af7f51bc0283c02d188bb9f1d
BeatyPete <akira941@gmail.com>
2021-04-21 02:25:00 -0500

    separated into pages and components

---
+++ client/src/components/Main-tabs/index.js
@@ -0,0 +1,26 @@
+import './main-tabs.css'
+
+function mainTabs() {
+    return (
+        <nav className="main-tabs">
+            <ul className="large-text">
+            <li className="selected">
+                <div>STAT</div>
+            </li>
+            <li>
+                <div>INV</div>
+            </li>
+            <li>
+                <div>DATA</div>
+            </li>
+            <li>
+                <div>MAP</div>
+            </li>
+            <li>
+                <div>RADIO</div>
+            </li>
+            </ul>
+        </nav>
+)}
+
+export default mainTabs;
