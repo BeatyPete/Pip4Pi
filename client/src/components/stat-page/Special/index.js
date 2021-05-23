28ab8380c43f9793c332d620ef88e80ca8cc7db8
BeatyPete <akira941@gmail.com>
2021-05-23 03:39:48 -0500

    added color changer temporarily to special tab colors now all change from there

--- client/src/components/stat-page/Special/index.js
+++ client/src/components/stat-page/Special/index.js
@@ -1,9 +1,16 @@
 /* import './status.css' */
 
 function Special() {
+    const colorChange = e => {
+        let hex = (e.target.value)
+        const rgb = ['0x' + hex[1] + hex[2] | 0, '0x' + hex[3] + hex[4] | 0, '0x' + hex[5] + hex[6] | 0];
+        let values = `${rgb[0]}, ${rgb[1]}, ${rgb[2]}`
+        document.documentElement.style.setProperty('--color', values);
+    }
+
     return (
     <main>
-        special
+        <input type="color" onChange={colorChange}></input>
     </main>
 )}
 
