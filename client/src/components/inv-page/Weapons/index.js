2079ac597f809a691f789d042e8c254d41f82461
BeatyPete <akira941@gmail.com>
2021-06-09 22:47:55 -0500

    filled out weapons page and styling for item list

--- client/src/components/inv-page/Weapons/index.js
+++ client/src/components/inv-page/Weapons/index.js
@@ -1,10 +1,26 @@
-/* import './status.css' */
+import './weapons.css'
 
 function Weapons() {
 
     return (
-    <main>
-        Weapons
+    <main className='flex-center'>
+        <div className='inventory-grid'>
+            <section className='small-text item-list'>
+                <ul>
+                    <li>Baseball Grenade (2)</li>
+                    <li>Cryo Mine</li>
+                    <li>Electrified Chinese Officer Sword</li>
+                    <li>Fragmentation Grenade</li>
+                    <li>Fragmentation Mine</li>
+                    <li>Suppressed Calibrated Powerful 10mm Pistol</li>
+                    <li>Suppressed Hardened Pipe Bolt-Action Sniper Rifle</li>
+                    <li>Two Shoy Boosted Automatic Laser Rifle</li>
+                </ul>
+            </section>
+            <section>
+
+            </section>
+        </div>
     </main>
 )}
 
