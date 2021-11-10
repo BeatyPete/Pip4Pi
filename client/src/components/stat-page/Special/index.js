e08a4f5384b6f3a3d630865835f845ecd2b2d19f
BeatyPete <akira941@gmail.com>
2021-11-09 22:37:34 -0600

    added placeholder pictures

--- client/src/components/stat-page/Special/index.js
+++ client/src/components/stat-page/Special/index.js
@@ -1,10 +1,11 @@
 /* import './status.css' */
+import placeholder from '../../../assets/images/special.jpg'
 
 function Special() {
 
     return (
     <main>
-        special
+        <div style={{width:'100%', height: '100%', background: `url(${placeholder}) 0% 0% / cover no-repeat var(--core-color)`, backgroundBlendMode: 'multiply'}}></div>
     </main>
 )}
 
