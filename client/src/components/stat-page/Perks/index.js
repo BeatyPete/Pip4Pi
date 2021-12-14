322297f44fe104633c663f6a8ffcab8a8d1cf275
BeatyPete <akira941@gmail.com>
2021-12-14 01:02:17 -0600

    created perks page

--- client/src/components/stat-page/Perks/index.js
+++ client/src/components/stat-page/Perks/index.js
@@ -1,10 +1,48 @@
-/* import './status.css' */
-import placeholder from '../../../assets/images/perks.jpg'
+import './perks.css'
+import { useState, useEffect } from 'react';
+import {perks} from '../../../lib/perks'
+import DefaultPerkAnim from '../../animations/perks/defaultPerk';
+import StarSvg from '../../images/star';
+import EmptyStarSvg from '../../images/emptyStar';
 
 function Perks() {
+    const [acquiredPerks] = useState([])
+    const [hoveredPerk, setHoveredPerk] = useState(0)
+
     return (
-        <main>
-            <div style={{width:'100%', height: '100%', background: `url(${placeholder}) 0% 0% / cover no-repeat var(--core-color)`, backgroundBlendMode: 'multiply'}}></div>
+      <main className='flex-center'>
+           <div className='main-wrapper small-text'>
+            <section className='list-container special-list'>
+                <ul>
+                {perks.map((perk, i) => (
+                    <li 
+                    /* ref={i === hoveredItem ? hovered: null} */
+                    /* className={`${addSelectedClass(i)} ${isHovered(i)}`} */
+                    key={`perks ${i}`}
+                    >
+                    
+                        <div>{perk.name}</div>
+                    </li>
+                ))}
+                </ul>
+            </section>
+            <section className='perk-info'>
+                <div className='flex-center perk-anim'>
+                    <DefaultPerkAnim></DefaultPerkAnim>
+                </div>
+                <div>
+                    <div className='flex-center'>
+                        {[...Array(perks[hoveredPerk].rank)].map((e, i) => <StarSvg classes='perk-rank'></StarSvg>)}
+                        {[...Array(perks[hoveredPerk].maxRank - perks[hoveredPerk].rank)].map((e, i) => <EmptyStarSvg classes='perk-rank'></EmptyStarSvg>)}
+                        {/* <StarSvg classes='perk-rank'></StarSvg>
+                        <EmptyStarSvg classes='perk-rank'></EmptyStarSvg> */}
+                    </div>
+                    <div className='perk-text'>
+                        {perks[hoveredPerk].description}
+                    </div>
+                </div>
+            </section>
+          </div>
       </main>
 )}
 
---
+++ client/src/lib/perks.js
@@ -0,0 +1,28 @@
+const perks = [
+    {
+        name: 'Armorer',
+        description: 'Protect yourself from the dangers of the Wasteland with access to base level and Rank 1 armor mods.',
+        rank: 1,
+        maxRank: 4 
+    },
+    {
+        name: 'Barbarian',
+        description: 'Protect yourself from the dangers of the Wasteland with access to base level and Rank 1 armor mods.',
+        rank: 2,
+        maxRank: 3 
+    },
+    {
+        name: 'Covert Operations',
+        description: 'Protect yourself from the dangers of the Wasteland with access to base level and Rank 1 armor mods.',
+        rank: 1,
+        maxRank: 4 
+    },
+    {
+        name: 'Gunslingers',
+        description: 'Protect yourself from the dangers of the Wasteland with access to base level and Rank 1 armor mods.',
+        rank: 1,
+        maxRank: 4 
+    }
+]
+
+module.exports = {perks};
